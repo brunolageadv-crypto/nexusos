68aa159f58ae795009d73ca6986539c28862b8f0
Bruno Lage de Freitas <brunolageadv@gmail.com>
2026-07-12 07:55:28 -0300

    Cache-bust dos icones (-v2) para atualizar taskbar

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = 'nexusos-v7'
+﻿const CACHE = 'nexusos-v8'
 const OFFLINE_URL = '/nexusos/'
 
 self.addEventListener('install', e => {
@@ -8,11 +8,11 @@ self.addEventListener('install', e => {
         '/nexusos/',
         '/nexusos/index.html',
         '/nexusos/manifest.json',
-        '/nexusos/favicon.svg',
-        '/nexusos/favicon.png',
-        '/nexusos/apple-touch-icon.png',
-        '/nexusos/icon-192.png',
-        '/nexusos/icon-512.png',
+        '/nexusos/favicon-v2.svg',
+        '/nexusos/favicon-v2.png',
+        '/nexusos/apple-touch-icon-v2.png',
+        '/nexusos/icon-192-v2.png',
+        '/nexusos/icon-512-v2.png',
       ]).catch(() => cache.add('/nexusos/'))
     }).then(() => self.skipWaiting())
   )
